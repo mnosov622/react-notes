1198ea663eb7444662ae45a72a2e06e567b4a54c
Maxim N <mnosov622@gmail.com>
2023-09-06 01:24:05 -0400

    styles update

--- todos/src/components/TodosCategory/TodosCategory.tsx
+++ todos/src/components/TodosCategory/TodosCategory.tsx
@@ -1,15 +1,37 @@
-import "./TodosCategory.css";
+import { useState } from "react";
 
 type Props = {
   setActive: (category: string) => void;
 };
 
 const TodosCategory = ({ setActive }: Props) => {
+  const [activeCategory, setActiveCategory] = useState("All"); // Initialize with the default active category
+
+  const handleSetActive = (category: string) => {
+    setActive(category);
+    setActiveCategory(category); // Update the active category state
+  };
+
   return (
     <section className="categories">
-      <button onClick={() => setActive("All")}>All</button>
-      <button onClick={() => setActive("Active")}>Active</button>
-      <button onClick={() => setActive("Completed")}>Completed</button>
+      <button
+        className={activeCategory === "All" ? "active" : ""}
+        onClick={() => handleSetActive("All")}
+      >
+        All
+      </button>
+      <button
+        className={activeCategory === "Active" ? "active" : ""}
+        onClick={() => handleSetActive("Active")}
+      >
+        Active
+      </button>
+      <button
+        className={activeCategory === "Completed" ? "active" : ""}
+        onClick={() => handleSetActive("Completed")}
+      >
+        Completed
+      </button>
     </section>
   );
 };
